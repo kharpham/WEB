175b3f86e1820bf9153c1a246a6bd9c83151fefa
unknown <khapn21411@st.uel.edu.vn>
2023-12-04 23:31:18 +0700

    11h30

--- interface/views.py
+++ interface/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "interface/commerce.html")
+    return render(request, "interface/countdown.html")
 
 def blog(request):
     return render(request, "interface/blog.html")
