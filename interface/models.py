74fdfc067c4c1f7fce328b54e75658a1f61a7348
unknown <khapn21411@st.uel.edu.vn>
2023-12-04 21:24:38 +0700

    Update 4/12

--- interface/models.py
+++ interface/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    category = models.CharField(max_length=64)
+    subcategory = models.CharField(max_length=64)
+    description = models.TextField()
+    image_url = models.URLField()
+
+class Blog(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    creator = models.CharField(max_length=50)
+    content = models.TextField()
+
+    
+    
