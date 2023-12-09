c6cbee2032fb609591ad33d3d5a2f6608e98d47e
unknown <khapn21411@st.uel.edu.vn>
2023-12-09 15:26:27 +0700

    asa

--- interface/admin.py
+++ interface/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Product, Blog, User
+from .models import Product, Blog
 
 # Register your models here.
 class BlogAdmin(admin.ModelAdmin):
     readonly_fields = ('timestamp',)
 admin.site.register(Product)
-admin.site.register(User)
 admin.site.register(Blog, BlogAdmin)
