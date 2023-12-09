989c9bf12eb06e8692e97c0f5012b160c9df76cd
unknown <khapn21411@st.uel.edu.vn>
2023-12-09 14:25:28 +0700

    user update2

--- Brownish/settings.py
+++ Brownish/settings.py
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'interface.User'
+AUTH_USER_MODEL = "interface.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- interface/admin.py
+++ interface/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product, Blog
+from .models import Product, Blog, User
 
 # Register your models here.
 class BlogAdmin(admin.ModelAdmin):
     readonly_fields = ('timestamp',)
 admin.site.register(Product)
+admin.site.register(User)
 admin.site.register(Blog, BlogAdmin)
